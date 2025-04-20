79319ca360eb46ae8f12946874835f2c601bfa67
AritraCh2005 <stfuaritra007@gmail.com>
2025-04-20 21:52:47 +0530

    usertabs fixes

--- src/components/layout/UserTabs.js
+++ src/components/layout/UserTabs.js
@@ -44,10 +44,48 @@
 //   );
 // }
 
-'use client';
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import ProfileImg from "../icons/ProfileImg";
+// 'use client';
+// import Link from "next/link";
+// import { usePathname } from "next/navigation";
+// import ProfileImg from "../icons/ProfileImg";
+
+// export default function UserTabs({ isAdmin }) {
+//   const path = usePathname();
+
+//   return (
+//     <div className="flex mx-auto gap-4 tabs justify-center flex-wrap p-4 rounded-lg shadow-lg">
+//       <Link
+//         className="px-4 py-2 rounded-md font-medium text-white bg-red-600"
+//         href={'/profile'}
+//       >
+        
+//           <ProfileImg />
+//       </Link>
+//       {isAdmin && (
+//         <>
+//           <Link
+//             href={'/categories'}
+//             className="px-4 py-2 rounded-md font-medium text-white bg-red-600"
+//           >
+//             Categories
+//           </Link>
+//           <Link
+//             href={'/menu-items'}
+//             className="px-4 py-2 rounded-md font-medium text-white bg-red-600"
+//           >
+//             Menu Items
+//           </Link>
+//           <Link
+//             className="px-4 py-2 rounded-md font-medium text-white bg-red-600"
+//             href={'/users'}
+//           >
+//             Users
+//           </Link>
+//         </>
+//       )}
+//     </div>
+//   );
+// }
 
 export default function UserTabs({ isAdmin }) {
   const path = usePathname();
@@ -58,32 +96,29 @@ export default function UserTabs({ isAdmin }) {
         className="px-4 py-2 rounded-md font-medium text-white bg-red-600"
         href={'/profile'}
       >
-        
-          <ProfileImg />
+        <ProfileImg />
       </Link>
-      {isAdmin && (
-        <>
-          <Link
-            href={'/categories'}
-            className="px-4 py-2 rounded-md font-medium text-white bg-red-600"
-          >
-            Categories
-          </Link>
-          <Link
-            href={'/menu-items'}
-            className="px-4 py-2 rounded-md font-medium text-white bg-red-600"
-          >
-            Menu Items
-          </Link>
-          <Link
-            className="px-4 py-2 rounded-md font-medium text-white bg-red-600"
-            href={'/users'}
-          >
-            Users
-          </Link>
-        </>
-      )}
+
+      <Link
+        href={'/categories'}
+        className="px-4 py-2 rounded-md font-medium text-white bg-red-600"
+      >
+        Categories
+      </Link>
+      <Link
+        href={'/menu-items'}
+        className="px-4 py-2 rounded-md font-medium text-white bg-red-600"
+      >
+        Menu Items
+      </Link>
+      <Link
+        className="px-4 py-2 rounded-md font-medium text-white bg-red-600"
+        href={'/users'}
+      >
+        Users
+      </Link>
     </div>
   );
 }
+
 
